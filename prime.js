a7f3766bba2f8a930e1917ecfd1b4f05bc468a90
Avinash Prakash <avinashprakash9122@gmail.com>
2023-10-25 23:55:12 +0530

    prime problem

---
+++ prime.js
@@ -0,0 +1,19 @@
+let num = 7; // Change this number to test for different values
+let isPrime = true;
+
+if (num <= 1) {
+    isPrime = false;
+} else {
+    for (let i = 3; i < num; i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
+
+if (isPrime) {
+    console.log(num + " is a prime number.");
+} else {
+    console.log(num + " is not a prime number.");
+}
