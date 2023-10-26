cfe6fdcc33a255204b87ff7cc4700d5915a79986
Avinash Prakash <avinashprakash9122@gmail.com>
2023-10-26 22:56:57 +0530

    my fix

--- prime.js
+++ prime.js
@@ -4,7 +4,7 @@ let isPrime = true;
 if (num <= 1) {
     isPrime = false;
 } else {
-    for (let i = 3; i < num; i++) {
+    for (let i = 1; i < num; i++) {
         if (num % i === 0) {
             isPrime = false;
             break;
